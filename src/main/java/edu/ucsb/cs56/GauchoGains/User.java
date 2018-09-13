18bc6d9efaa602caf94ffd9fb10737d91072e746
Maga Kim <maga@umail.ucsb.edu>
2018-09-13 14:45:57 -0700

    Separated valid user check to User class

--- src/main/java/edu/ucsb/cs56/GauchoGains/User.java
+++ src/main/java/edu/ucsb/cs56/GauchoGains/User.java
@@ -1,6 +1,7 @@
 package edu.ucsb.cs56.GauchoGains;
 
 public class User {
+	private String valid;
 	private String email;
 	private String firstName;
 	private String lastName;
@@ -12,6 +13,7 @@ public class User {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.password = password;
+		checkValidUser();
 	}
 	public String getFirstName() {
 		return this.firstName;
@@ -30,17 +32,34 @@ public class User {
 		this.firstName = firstName;
 	}
 	public void setLastName(String lastName) {
-                this.lastName = lastName;
-        }
+		this.lastName = lastName;
+	}
 	public void setEmail(String email) {
-                this.email = email;
-        }
+		this.email = email;
+	}
 	public void setPassword(String password) {
-                this.password = password;
-        }
+		this.password = password;
+	}
+
+	private void checkValidUser() {
+		if(this.email.length() == 0 || this.firstName.length() == 0 || this.lastName.length() == 0 || this.password.length() == 0)
+			this.valid = "Please fill in all forms";
+		else if(!this.email.contains("@") || this.email.length() < 5 || this.email.charAt(email.length()-4) != '.' ||
+				this.email.charAt(email.indexOf("@")+1) == '.')
+			this.valid = "Invalid Email";
+		else if(this.password.length() < 6)
+			this.valid = "Please enter a password with length greater than 6";
+		else 
+			this.valid = "valid";
+	}
 	
+	public String getValidUserCheck() {
+		return this.valid;
+	}
+
 	@Override
 	public String toString() {
 		return this.email + " " + this.firstName + " " + this.lastName;
 	}
+
 }
